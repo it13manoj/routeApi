e50459666845992f65e4ea9e45c1ed35684852d7
manoj sharma <it13manoj@gmail.com>
2024-01-01 23:48:53 +0530

    update

--- src/main/java/routers/com/controller/Routes.java
+++ src/main/java/routers/com/controller/Routes.java
@@ -1,100 +1,134 @@
 package routers.com.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import localsolver.LSExpression;
 import localsolver.LSModel;
+import localsolver.LSSolutionStatus;
 import localsolver.LocalSolver;
+import localsolver.modeler.LSPMap;
+import localsolver.modeler.LSPModeler;
+import localsolver.modeler.LSPModule;
 
 
 @RestController
 @RequestMapping("/api/v1")
 public class Routes {
-	 private static final double PI = 3.14159265359;
-	 private final LocalSolver localSolver ;
+	 private static final String UPLOAD_DIR = "./uploads"; 
+	 public static final String LSP_PATH = "./static/uploads/model.lsp";
 	 
-	 	private LSExpression R;
-	    private LSExpression r;
-	    private LSExpression h;
+	 private final ResourceLoader resourceLoader;
 
-	    private LSExpression surface;
-	    private LSExpression volume;
-	    
-	    private Routes(LocalSolver localsolver) {
-	        this.localSolver = localsolver;
+	 
+	 @Autowired
+	    public Routes(ResourceLoader resourceLoader) {
+	        this.resourceLoader = resourceLoader;
 	    }
-	    
- private void solve(int limit) {
+
+	 
+	 
+	 	@PostMapping("/upload")
+		public ResponseEntity<byte[]> getRoutes(@RequestParam("file") MultipartFile file) throws IOException {
+	 		String fileName = "model.lsp"; 
+	 		String jsonContent;
+	        String filePath;
+			
+				filePath = resourceLoader.getResource("classpath:static/uploads/" + fileName).getFile().getAbsolutePath();
+			
+				LSPModeler modeler = new LSPModeler();
+	 			String lspPath = LSP_PATH;
+
+	 			
+	 		
+	         	LSPModule main = modeler.loadModule(filePath);
+	         	LocalSolver solver = modeler.createSolver();
+	         	 byte[] bytes = file.getBytes();
+	             File uploadDir = new File(UPLOAD_DIR);
+	             if (!uploadDir.exists()) {
+	                 uploadDir.mkdirs();
+	             }
+	            
+	        	 Path path = Paths.get(uploadDir.getAbsolutePath() + File.separator + file.getOriginalFilename());
+	             Files.write(path, bytes);
+	             String jsonPath = new String(Files.readAllBytes(path));
+	             main.setString("inFileName", jsonPath);
+		         main.run(solver);
+		         LSSolutionStatus solutionStatus = solver.getSolution().getStatus();
+		        	
+		        	if (solutionStatus == LSSolutionStatus.Infeasible || solutionStatus == LSSolutionStatus.Inconsistent) {
+		        		jsonContent = "";
+		        	}
+		        	
+		        	jsonContent = displaySolution(main);
+		        	byte[] bytes1 = new ObjectMapper().writeValueAsBytes(jsonContent);
+
+		        	 HttpHeaders headers = new HttpHeaders();
+		             headers.setContentType(MediaType.APPLICATION_JSON);
+		             headers.setContentDispositionFormData("attachment", "data.json"); // Set file name for download
+
+		             return ResponseEntity.ok().headers(headers).body(bytes);
+			
+		}
+	 	
+		public String displaySolution(LSPModule main) {
+			 String json;
+	    	ArrayList<Map> arraylist = new ArrayList<>();
 	    	
-	        // Declare the optimization model
-	        LSModel model = localSolver.getModel();
-	        LSExpression piConst = model.createConstant(PI);
+	    	LSPMap sites = main.getMap("outputTasks");
+	    	for(int i =0 ; i < sites.count(); i++ ) {
+	    		Map map = new HashMap();
+	    		map.put("name", sites.getMap(i).getString("name"));
+	    		map.put("truck", sites.getMap(i).getString("resource"));
+	    		map.put("color", sites.getMap(i).getString("color"));
+	    		map.put("startTime", sites.getMap(i).getInt("startTime"));
+	    		map.put("endTime", sites.getMap(i).getInt("endTime"));
+	    		arraylist.add(map);
+	    	}
+	    	
+	    	ObjectMapper objectMapper = new ObjectMapper();
+	    	 
+	               try {
+					json = objectMapper.writeValueAsString(arraylist);
+					
+				} catch (JsonProcessingException e) {
+					// TODO Auto-generated catch block
+					json = "";
+				}
+	    	  return json;
+		}
 
-	        // Numerical decisions
-	        R = model.floatVar(0, 1);
-	        r = model.floatVar(0, 1);
-	        h = model.floatVar(0, 1);
 
-	        // surface = PI*r^2 + PI*(R+r)*sqrt((R - r)^2 + h^2)
-	        LSExpression s1 = model.prod(piConst, r, r);
-	        LSExpression s2 = model.pow(model.sub(R, r), 2);
-	        LSExpression s3 = model.pow(h, 2);
-	        LSExpression s4 = model.sqrt(model.sum(s2, s3));
-	        LSExpression s5 = model.sum(R, r);
-	        LSExpression s6 = model.prod(piConst, s5, s4);
-	        surface = model.sum(s1, s6);
 
-	        // Surface must not exceed the surface of the plain disc
-	        model.addConstraint(model.leq(surface, PI));
-
-	        LSExpression v1 = model.pow(R, 2);
-	        LSExpression v2 = model.prod(R, r);
-	        LSExpression v3 = model.pow(r, 2);
-
-	        // volume = PI*h/3*(R^2 + R*r + r^2)
-	        volume = model.prod(piConst, model.div(h, 3), model.sum(v1, v2, v3));
-
-	        // Maximize the volume
-	        model.maximize(volume);
-
-	        model.close();
-
-	        // Parametrize the solver
-	        localSolver.getParam().setTimeLimit(limit);
-
-	        localSolver.solve();
-	    }
- 
- private void writeSolution(String fileName) throws IOException {
-     try (PrintWriter output = new PrintWriter(fileName)) {
-         output.println(surface.getDoubleValue() + " " + volume.getDoubleValue());
-         output.println(R.getDoubleValue() + " " + r.getDoubleValue() + " " + h.getDoubleValue());
-     }
- }
-
-	
-	@GetMapping("/find")
-	public String findRoutes() {
-		
-		 String outputFile = "";
-	        String strTimeLimit = "2";
-
-	        try (LocalSolver localsolver = new LocalSolver()) {
-	        	Routes model = new Routes(localsolver);
-	            model.solve(Integer.parseInt(strTimeLimit));
-	            if (outputFile != null) {
-	                model.writeSolution(outputFile);
-	            }
-	        } catch (Exception ex) {
-	            System.err.println(ex);
-	            ex.printStackTrace();
-	            System.exit(1);
-	        }
-		return null;
 	}
-}
